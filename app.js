7e3527e12e259affb378e5a805565385feff3e5a
Daniel Cenoz <cuervodaniel@rocketmail.com>
2020-02-11 15:52:09 -0300

    router

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
+//server config
 const conf = require('./configs/server')
-
 const app = conf.app()
 const port = conf.port()
 const express = conf.express()
+
 //sets
 app.set('view engine','pug')
 app.use(express.static('public'))
-//app.use(app.static('public'))
+app.use(require('./routes/routes'))
 
-app.get('/', (req, res) => res.render('index',{title:'Tech for Humans'}))
-
+//server
 app.listen(port, () => console.log(`Servidor escuchando puerta ${port}!`))
---
+++ routes/routes.js
@@ -0,0 +1,11 @@
+
+const r = require('../configs/server').express()
+
+const router = r.Router()
+router.get('/', (req, res) => res.render('index',{title:'Tech for Humans'}))
+router.get('/about', (req, res) => res.send('About'))
+
+//404
+router.use((req, res) => res.status(404).send('404'))
+
+module.exports = router
